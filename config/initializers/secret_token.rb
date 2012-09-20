12aae0456525dd8b8912c5c896ec57a39b7e2e53
jaycevdl <jayce@vanstudiophotography.com>
2012-09-20 00:30:40 -0600

    setting up the base
    just getting the base of the blog built

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ForksAndTent::Application.config.secret_token = '4885c61dac7e9f63108154458b5e8dbd8f1834461575682e36a5d3c13fe943dcd8f84a1ed0fe0faae469e568fb30de646e17136e1d24ef89234886fb8e5f94ee'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+ForksAndTent::Application.config.session_store :cookie_store, :key => '_Forks-and-Tent_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# ForksAndTent::Application.config.session_store :active_record_store
